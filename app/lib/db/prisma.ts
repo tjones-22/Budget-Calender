f88e2452177cff85d665317a1f4abd218dff8c73
tristanjones-student <tristanjones@myboisestate.edu>
2026-07-26 07:42:35 -0600

    switched from sqlite to postgres for vercel deployment

--- app/lib/db/prisma.ts
+++ app/lib/db/prisma.ts
@@ -1,12 +1,12 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL ?? "file:./prisma/app.db",
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
 });
 
 export const prisma =
